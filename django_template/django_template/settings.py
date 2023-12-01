b2448647db4044e3a87e97bd0b7da13703533bcf
Hồ Đức An <98076022+HoAn248@users.noreply.github.com>
2023-12-01 10:25:34 +0700

    django template static file

--- django_template/django_template/settings.py
+++ django_template/django_template/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
+BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
 
 # Default primary key field type
